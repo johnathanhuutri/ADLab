7f984569420dcbba053c4642a624247727cff102
johnathanhuutri <nhtri2003@gmail.com>
2024-10-20 08:50:46 +0700

    Update

--- module2/central.sh
+++ module2/central.sh
@@ -5,16 +5,21 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-if [ ! -z "ForcAD_v1.4.0.zip" ];
+if [ ! -f "ForcAD_v1.4.0.zip" ];
 then
 	echo "Missing ForcAD_v1.4.0.zip"
 	exit
 fi
 
+install_basic_stuff() {
+	sudo apt-get update
+	sudo apt-get install -y build-essential unzip
+}
+
 install_docker() {
 	# Add Docker's official GPG key:
 	sudo apt-get update
-	sudo apt-get install build-essential ca-certificates curl
+	sudo apt-get install -y ca-certificates curl
 	sudo install -m 0755 -d /etc/apt/keyrings
 	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 	sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -32,7 +37,7 @@ setup_forcad() {
 	mv ForcAD_v1.4.0 /ForcAD
 }
 
-apt remove unattended-upgrades
+apt remove -y unattended-upgrades
 install_docker
 setup_forcad
 
