f7e40cf2a5905492fa6ed404d8e87685263e7abc
johnathanhuutri <nhtri2003@gmail.com>
2024-11-16 22:44:20 +0700

    [mod2] Update script

--- mod2/team.sh
+++ mod2/team.sh
@@ -41,6 +41,15 @@ network_configuration() {
                 addresses: [8.8.8.8, 8.8.4.4]""" > "/etc/netplan/01-client-network.yaml"
 	chmod 600 "/etc/netplan/01-client-network.yaml"
 	netplan apply
+
+	# Check internet
+	wget -q --spider http://google.com
+
+	if [ $? -eq 0 ]; then
+		echo "[*] Network config successful"
+	else
+		echo "[-] Network config failed"
+	fi
 }
 
 basic_setup() {
